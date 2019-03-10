7883b70b031711b61ac6d99349b5f81a23a18ff1
apatronl <ale7feb@hotmail.com>
2019-03-10 18:33:54 -0400

    Add link to source code in Settings

--- Hackr/ViewController/Settings/SettingsViewController.swift
+++ Hackr/ViewController/Settings/SettingsViewController.swift
@@ -13,8 +13,11 @@ final class SettingsViewController: UIViewController {
   // MARK: - Constants
 
   enum Constants {
+    static let hackrSourceCodeUrl = URL(string: "https://github.com/apatronl/Hackr")
     static let interfaceTitle = "Interface"
-    static let hackrSettingNames = ["Dark Mode"]
+    static let interfaceSettingNames = ["Dark Mode"]
+    static let sourcesSettingsNames = ["Source Code"]
+    static let sections = 2
   }
 
   // MARK: - Private Properties
@@ -75,11 +78,27 @@ final class SettingsViewController: UIViewController {
 // MARK: - UITableViewDelegate
 
 extension SettingsViewController: UITableViewDelegate {
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath, animated: true)
+    switch indexPath.section {
+    case 1:
+      guard let safariView =
+        safariViewForItem(at: Constants.hackrSourceCodeUrl, defaultUrl: nil) else { return }
+      present(safariView, animated: true, completion: nil)
+    default:
+      break
+    }
+  }
 }
 
 // MARK: - UITableViewDataSource
 
 extension SettingsViewController: UITableViewDataSource {
+
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return Constants.sections
+  }
+
   func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
     switch section {
     case 0:
@@ -92,7 +111,9 @@ extension SettingsViewController: UITableViewDataSource {
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     switch section {
     case 0:
-      return Constants.hackrSettingNames.count
+      return Constants.interfaceSettingNames.count
+    case 1:
+      return Constants.sourcesSettingsNames.count
     default:
       return 0
     }
@@ -100,19 +121,26 @@ extension SettingsViewController: UITableViewDataSource {
 
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
-    cell.textLabel?.text = Constants.hackrSettingNames[indexPath.row]
     cell.textLabel?.textColor = darkModeState == .on ? .white : .black
     cell.backgroundColor = darkModeState == .on ? UIColor.darkModeGray : .white
-    switch indexPath.row {
+    // If more rows are added to one of these sections, then we also need to check indexPath.row
+    // when creating the cell
+    switch indexPath.section {
     case 0:
+      cell.textLabel?.text = Constants.interfaceSettingNames[indexPath.row]
       cell.accessoryView = setUpDarkModeSwitch()
       cell.selectionStyle = .none
+    case 1:
+      cell.textLabel?.text = Constants.sourcesSettingsNames[indexPath.row]
+      cell.accessoryType = .disclosureIndicator
     default:
       break
     }
     return cell
   }
 }
+
+// MARK: - DarkModeDelegate
 
 extension SettingsViewController: DarkModeDelegate {
   func darkModeStateDidChange(_ state: DarkModeState) {
